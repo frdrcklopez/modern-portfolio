1c1255aea787a2104257f4971cf6aa33965f8166
effs12 <fredericklopez12@gmail.com>
2022-09-18 02:13:15 -0400

    createad the Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       {/* Header */}
+      <Header />
 
       {/* Hero */}
 
