c4b0f1313f66a8aada2c3c835312edac602f0b57
effs12 <fredericklopez12@gmail.com>
2022-09-18 08:35:55 -0400

    Created the hero button on hero component and heroButton css on globals layer component

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -22,7 +22,7 @@ const BackgroundCircle = ({} : Props) => {
             <div className='absolute border border-gray-700 rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
             <div className='absolute border border-gray-700 rounded-full h-[300px] w-[300px] mt-52'/>
             <div className='absolute border border-gray-700 rounded-full h-[500px] w-[500px] mt-52'/>
-            <div className='absolute border border-orange-500 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
+            <div className='absolute border border-orange-500/80 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
             <div className='absolute border border-gray-700 rounded-full h-[800px] w-[800px] mt-52 animate-pulse'/>
         </motion.div>
     );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 import BackgroundCircle from './BackgroundCircles'
@@ -23,12 +24,27 @@ const Hero = ({} : Props) => {
                 className='relative rounded-full h-32 w-32 mx-auto object-cover'
                 src="https://avatars.githubusercontent.com/u/24982705?v=4"
             />
-            <div>
-                <h2>Software Engineer</h2>
-                <h1>
+            <div className='z-20'>
+                <h2 className='text-sm uppecase text-gray-500 pb-2 tracking[-15px]'>Full Stack Web Developer</h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
                     <span>{text}</span>
                     <Cursor cursorColor='#F7AB0A'/>
                 </h1>
+
+                <div className='pt-5'>
+                    <Link href="#about">
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href="#projects">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
