fa34fa01c761b561b92f43fef34323cc11c3cc2e
effs12 <fredericklopez12@gmail.com>
2022-09-19 01:39:33 -0400

    Created the about component

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+type Props = {};
+
+const About = ({} : Props) => {
+    return (
+        <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+        </div>
+    );
+}
+ 
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 const Home: NextPage = () => {
   return (
@@ -16,11 +17,14 @@ const Home: NextPage = () => {
       <Header />
 
       {/* Hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero/>
       </section>
 
       {/* About */}
+      <section id="about" className='snap-center'>
+        <About/>
+      </section>
 
       {/* Experience */}
 
