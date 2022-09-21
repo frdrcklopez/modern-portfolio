ecda354f935cdaf3ab287b42b7c35f1c0534a842
effs12 <fredericklopez12@gmail.com>
2022-09-21 04:30:42 -0400

    import the skills component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
+import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
@@ -33,6 +34,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* Skills */}
+      <section id="skills" className='snap-start'>
+        <Skills/>
+      </section>
 
       {/* Projects */}
 
