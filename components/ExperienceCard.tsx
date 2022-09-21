5d8fdd8268e2938397bbd89cd8f2e090e730c7a9
effs12 <fredericklopez12@gmail.com>
2022-09-21 04:29:51 -0400

    created the experienced card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const ExperienceCard = ({} : Props) => {
     return (
-        <article>
+        <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] snap-center bg-neutral-700 p-10 opacity-50 hover:opacity-100 cursor-pointer'>
             <motion.img 
                 initial={{
                     y:-100,
@@ -21,9 +21,36 @@ const ExperienceCard = ({} : Props) => {
                 viewport={{
                     once: true
                 }}
-                className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+                className='w-28 h-28 rounded-full object-cover object-center'
                 src="https://avatars.githubusercontent.com/u/24982705?v=4"
             />
+            <div>
+                <h4 className='text-4xl font-light'>CEO of PAPAFAM</h4>
+                <p className='font-bold text-2xl mt-1'>PAPAFARM</p>
+                <div className='flex space-x-2 my-2'>
+                        <img 
+                            className='h-10 w-10 rounded-full'
+                            src="https://cdn.sanity.io/images/ltuexkre/production/fa5e6eeb3377a1601e181d0eeb9a60633090cff2-500x500.png"
+                        />
+                        <img 
+                            className='h-10 w-10 rounded-full'
+                            src="https://cdn.sanity.io/images/ltuexkre/production/fa5e6eeb3377a1601e181d0eeb9a60633090cff2-500x500.png"
+                        />
+                        <img 
+                            className='h-10 w-10 rounded-full'
+                            src="https://cdn.sanity.io/images/ltuexkre/production/fa5e6eeb3377a1601e181d0eeb9a60633090cff2-500x500.png"
+                        />
+                </div>
+                <p className='uppercase py-5 text-gray-300'>Started work... - Ended...</p>
+
+                <ul className='list-disc space-y-4 ml-5 text-base'>
+                    <li>Sumary points Sumary points Sumary points Sumary points</li>
+                    <li>Sumary points Sumary points Sumary points Sumary points</li>
+                    <li>Sumary points Sumary points Sumary points Sumary points</li>
+                    <li>Sumary points Sumary points Sumary points Sumary points</li>
+                    <li>Sumary points Sumary points Sumary points Sumary points</li>
+                </ul>
+            </div>
         </article>
     );
 }
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -16,10 +16,10 @@ const WorkExperience = ({} : Props) => {
             transition={{
                 duration : 1.5
             }}
-            className='flex flex-col relative h-screen overflow-hidden text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center border'
+            className='flex flex-col relative h-screen overflow-hidden text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
         >
             <h3 className='absolute top-14 uppercase tracking-[20px] text-gray-500 text-2xl -mr-[40px]'>Experience</h3>
-            <div>
+            <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
                 {/* ExperienceCard */}
                 <ExperienceCard/>
                 {/* ExperienceCard */}
