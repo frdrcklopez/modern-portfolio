84e8b333e4844991c7bc6e570738fd2a27630ad3
effs12 <fredericklopez12@gmail.com>
2022-09-22 07:18:35 -0400

    Created the project section component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 const Home: NextPage = () => {
   return (
@@ -39,6 +40,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* Projects */}
+      <section id="projects" className='snap-start'>
+        <Projects/>
+      </section>
 
       {/* Contact Me */}
 
