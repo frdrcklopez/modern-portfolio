b4707141883163f85362b0cc55b30ff6b91aef3d
effs12 <fredericklopez12@gmail.com>
2022-09-22 07:18:15 -0400

    set the header from sticky to fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Header = ({} : Props) => {
     return (
-        <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+        <header className='fixed w-full top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
             <motion.div
                 initial={{
                     x : -500,
