303157940060af50af185081d89ea165ad25308d
effs12 <fredericklopez12@gmail.com>
2022-09-25 15:04:18 -0400

    Applied the SectionFlexBox in ContactMe section component

--- modules/sections/ContactMe.tsx
+++ modules/sections/ContactMe.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import SectionFlexBox from '../components/layout/SectionFlexBox'
 
 type Inputs = {
     name : string,
@@ -25,58 +26,55 @@ const ContactMe = ({} : Props) => {
     }
 
     return (
-        <motion.div 
-            initial={{
-                opacity : 0
-            }}
-            whileInView={{
-                opacity : 1
-            }}
-            transition={{
-                duration : 1.5
-            }}
-            className='flex flex-col relative min-h-screen text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 justify-center xl:space-y-0 mx-auto items-center'
-        >
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl -mr-[40px]'>
-                Contact
-            </h3>
-            
-            <div className='flex flex-col space-y-10'>
-                <h4 className='text-4xl font-semibold text-center'>
-                    I have got what you need. <span className='decoration-amber-500 underline'>Lets talk.</span>
-                </h4>
+        <SectionFlexBox title="contact">
+            <motion.div 
+                initial={{
+                    opacity : 0
+                }}
+                whileInView={{
+                    opacity : 1
+                }}
+                transition={{
+                    duration : 1.5
+                }}
+                className='h-full w-full'
+            >
+                <div className='flex flex-col py-10'>
+                    <h4 className='text-4xl font-semibold text-center mb-10'>
+                        I have got what you need. <span className='decoration-amber-500 underline'>Lets talk.</span>
+                    </h4>
 
-                <div className='space-y-10'>
-                    <div className='flex items-center space-x-5 justify-center'>
-                        <PhoneIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
-                        <p className='text-2xl'>+123 456 789</p>
+                    <div className='space-y-5'>
+                        <div className='flex items-center space-x-5 justify-center'>
+                            <PhoneIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
+                            <p className='text-2xl'>+123 456 789</p>
+                        </div>
+
+                        <div className='flex items-center space-x-5 justify-center'>
+                            <EnvelopeIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
+                            <p className='text-2xl'>fredericklopez@gmail.com</p>
+                        </div>
+
+                        <div className='flex items-center space-x-5 justify-center'>
+                            <MapPinIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
+                            <p className='text-2xl'>Niagara Falls, Ontario, Canada</p>
+                        </div>
                     </div>
 
-                    <div className='flex items-center space-x-5 justify-center'>
-                        <EnvelopeIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
-                        <p className='text-2xl'>fredericklopez@gmail.com</p>
-                    </div>
 
-                    <div className='flex items-center space-x-5 justify-center'>
-                        <MapPinIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
-                        <p className='text-2xl'>Niagara Falls, Ontario, Canada</p>
-                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto py-10'>
+                        <div className='flex space-x-2'>
+                            <input {...register('name')} type="text" className='contactInput'/>
+                            <input {...register('email')} type="text" className='contactInput'/>
+                        </div>
+
+                        <input {...register('subject')} className='contactInput' type="text"/>
+                        <textarea {...register('message')} className="contactInput" />
+                        <button className="bg-amber-500 py-5 px-10 rounded-md text-black font-bold">Submit</button>
+                    </form>
                 </div>
-
-
-                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
-                    <div className='flex space-x-2'>
-                        <input {...register('name')} type="text" className='contactInput'/>
-                        <input {...register('email')} type="text" className='contactInput'/>
-                    </div>
-
-                    <input {...register('subject')} className='contactInput' type="text"/>
-                    <textarea {...register('message')} className="contactInput" />
-                    <button className="bg-amber-500 py-5 px-10 rounded-md text-black font-bold">Submit</button>
-                </form>
-            </div>
-
-        </motion.div>
+            </motion.div>
+        </SectionFlexBox>
     );
 }
  
