9afa1cd665a89f7a88af1955cc1699cdcbf990bd
effs12 <fredericklopez12@gmail.com>
2022-09-25 15:04:06 -0400

    Applied the SectionFlexBox in WorkExperience section component

--- modules/sections/WorkExperience.tsx
+++ modules/sections/WorkExperience.tsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import SectionFlexBox from '../components/layout/SectionFlexBox'
 import ExperienceCard from '../components/ExperienceCard'
 
 type Props = {};
 
 const WorkExperience = ({} : Props) => {
     return (
-        <motion.div 
-            initial={{
-                opacity : 0
-            }}
-            whileInView={{
-                opacity : 1
-            }}
-            transition={{
-                duration : 1.5
-            }}
-            className='flex flex-col relative h-screen overflow-hidden text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
-        >
-            <h3 className='absolute top-14 uppercase tracking-[20px] text-gray-500 text-2xl -mr-[40px]'>Experience</h3>
-            <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
-                {/* ExperienceCard */}
-                <ExperienceCard/>
-                {/* ExperienceCard */}
-                <ExperienceCard/>
-                {/* ExperienceCard */}
-                <ExperienceCard/>
-                {/* ExperienceCard */}
-                <ExperienceCard/>
-            </div>
-        </motion.div>
+        <SectionFlexBox title="Experience">
+            <motion.div 
+                initial={{
+                    opacity : 0
+                }}
+                whileInView={{
+                    opacity : 1
+                }}
+                transition={{
+                    duration : 1.5
+                }}
+                className='w-full h-full'
+            >
+                <div className='h-full w-full flex space-x-5 overflow-x-scroll px-10 snap-x snap-mandatory'>
+                    {/* ExperienceCard */}
+                    <ExperienceCard/>
+                    {/* ExperienceCard */}
+                    <ExperienceCard/>
+                    {/* ExperienceCard */}
+                    <ExperienceCard/>
+                    {/* ExperienceCard */}
+                    <ExperienceCard/>
+                </div>
+            </motion.div>
+        </SectionFlexBox>
     );
 }
  
