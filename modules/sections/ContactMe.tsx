96381d3f1dfa3969fb5566fe836a91acffc6f5a5
effs12 <fredericklopez12@gmail.com>
2022-09-26 10:26:27 -0400

    Adjust the form spacing in ContactMe section component

--- modules/sections/ContactMe.tsx
+++ modules/sections/ContactMe.tsx
@@ -22,7 +22,7 @@ const ContactMe = ({} : Props) => {
     } = useForm<Inputs>();
 
     const onSubmit : SubmitHandler<Inputs> = (formData) => {
-        // console.log(formData)
+        console.log(formData)
     }
 
     return (
@@ -37,40 +37,41 @@ const ContactMe = ({} : Props) => {
                 transition={{
                     duration : 1.5
                 }}
-                className='h-full w-full'
+                className='h-full w-full flex justify-center'
             >
-                <div className='flex flex-col py-10'>
-                    <h4 className='text-4xl font-semibold text-center mb-10'>
+                <div className='flex flex-col space-y-2 sm:space-y-3 2xl:space-y-5 py-5 sm:py-8 2xl:py-16'>
+                    <h4 className='text-xl sm:text-2xl 2xl:text-4xl font-semibold text-center mb-5 2xl:mb-10'>
                         I have got what you need. <span className='decoration-amber-500 underline'>Lets talk.</span>
                     </h4>
 
-                    <div className='space-y-5'>
+                    <div className='space-y-2 sm:space-y-3 2xl:space-y-5'>
                         <div className='flex items-center space-x-5 justify-center'>
-                            <PhoneIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
-                            <p className='text-2xl'>+123 456 789</p>
+                            <PhoneIcon className="text-amber-500 h-4 sm:h-6 2xl:h-7 w-4 sm:w-6 2xl:w-7 animate-pulse"/>
+                            <p className='text-base sm:text-xl 2xl:text-2xl'>+123 456 789</p>
                         </div>
 
                         <div className='flex items-center space-x-5 justify-center'>
-                            <EnvelopeIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
-                            <p className='text-2xl'>fredericklopez@gmail.com</p>
+                            <EnvelopeIcon className="text-amber-500 h-4 sm:h-6 2xl:h-7 w-4 sm:w-6 2xl:w-7 animate-pulse"/>
+                            <p className='text-base sm:text-xl 2lx:text-2xl'>youremail@gmail.com</p>
                         </div>
 
                         <div className='flex items-center space-x-5 justify-center'>
-                            <MapPinIcon className="text-amber-500 h-7 w-7 animate-pulse"/>
-                            <p className='text-2xl'>Niagara Falls, Ontario, Canada</p>
+                            <MapPinIcon className="text-amber-500 h-4 sm:h-6 2xl:h-7 w-4 sm:w-6 2xl:w-7 animate-pulse"/>
+                            <p className='text-base sm:text-xl 2xl:text-2xl'>Niagara Falls, Ontario, Canada</p>
                         </div>
                     </div>
 
-
                     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto py-10'>
                         <div className='flex space-x-2'>
-                            <input {...register('name')} type="text" className='contactInput'/>
-                            <input {...register('email')} type="text" className='contactInput'/>
+                            <input {...register('name')} type="text" className='contactInput' placeholder='Name'/>
+                            <input {...register('email')} type="text" className='contactInput' placeholder='Email'/>
                         </div>
 
-                        <input {...register('subject')} className='contactInput' type="text"/>
-                        <textarea {...register('message')} className="contactInput" />
-                        <button className="bg-amber-500 py-5 px-10 rounded-md text-black font-bold">Submit</button>
+                        <input {...register('subject')} className='contactInput' type="text" placeholder='Subject'/>
+                        <textarea {...register('message')} className="contactInput" placeholder='Message'/>
+                        <button className="bg-amber-500 py-2 sm:py-3 2xl:py-4 rounded-md text-black font-bold">
+                            Submit
+                        </button>
                     </form>
                 </div>
             </motion.div>
