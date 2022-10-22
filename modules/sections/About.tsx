e40f64fff33942e0dde2f8dc4f913696865e0d19
effs12 <fredericklopez12@gmail.com>
2022-10-22 03:33:36 -0400

    dispaly the pageInfo on about sections

--- modules/sections/About.tsx
+++ modules/sections/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import SectionFlexBox from '../components/layout/SectionFlexBox'
+import { urlFor } from '../../sanity'
 import { PageInfo } from '../../typing'
 
 type Props = {
@@ -24,7 +25,7 @@ const About = ({ pageinfo } : Props) => {
                             once : true 
                         }}
                         className='h-32 w-32 sm:h-44 sm:w-44 rounded-full object-cover md:rounded-lg md:w-64 lg:w-full md:h-auto aspect-[3/3.5]'
-                        src="https://avatars.githubusercontent.com/u/24982705?v=4"
+                        src={urlFor(pageinfo?.heroImage).url()}
                     />
                 </div>
                 <div className='p-5 w-full flex flex-col justify-center items-center sm:items-start space-y-5 md:space-y-10'>
@@ -32,7 +33,7 @@ const About = ({ pageinfo } : Props) => {
                         Here is a <span className='underline decoration-amber-500'>little</span> background
                     </h4>
                     <p className='text-sm sm:text-base lg:text-lg'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                        {pageinfo.backgroundInformation}
                     </p>
                 </div>
             </div>
